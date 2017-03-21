af72759290efe48728e13cad9eb3756c776b8527
Eric Kinzel <ericrkinzel@gmail.com>
2017-03-21 17:43:07 +0000

    =set up user ajax

--- js/components/app.js
+++ js/components/app.js
@@ -1,12 +1,37 @@
-define(["backbone", "router"],
-function(Backbone, Router) {
+define(["backbone", "router", "user_service", "jquery"],
+function(Backbone, Router, User, $) {
     
     var initFx = function(){
         Backbone.history.start();
+        app.ajaxSetup();
+        User.getCurrentUser();
     };
     
+    var ajaxSetup = function(){
+        $.ajaxSetup({
+            statusCode: {
+                401: function(){
+                    User.clearCurrentUser();
+                    Router.navigate("login", {trigger: true});
+                },
+                403: function(){
+                    User.clearCurrentUser();
+                    Router.navigate("login", {trigger: true});
+                }
+            }, headers: {
+                Authorization: app.getToken()
+            }
+        })
+    };
+    
+    var getToken = function(){
+        return window.localStorage.getItem("token") || ""
+    }
+    
     var app = {
-        init: initFx
+        init: initFx,
+        ajaxSetup: ajaxSetup,
+        getToken: getToken
     };
     
     return app;
