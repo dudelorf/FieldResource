27dfe3891cc77466afa8b0469f05d0326b7fadac
Eric <ericrkinzel@gmail.com>
2017-04-14 18:04:08 -0500

    =added navigaton to home view, added scripts for search and jobs

--- js/components/require-main.js
+++ js/components/require-main.js
@@ -22,11 +22,24 @@ require.config({
         home_view: "components/home/views/home",
         
         admin_controller: "components/admin/admin-controller",
-        admin_view: "components/admin/views/admin"
+        admin_view: "components/admin/views/admin",
+        
+        job_controller: "components/jobs/jobs-controller",
+        job_models: "components/jobs/jobs-models",
+        job_view: "components/jobs/views/job",
+        job_new_view: "components/jobs/views/new-job",
+        
+        search_controller: "components/search/search-controller.js",
+        search_models: "components/search/search-models.js",
+        search_form: "components/search/views/search-form.js",
+        search_results: "components/search/views/search-results.js"
     },
     shim: {
         "bootstrap": {
             deps: ["jquery"]
+        },
+        "backbone": {
+            deps: ["underscore", "jquery"]
         }
     },
     waitSeconds: 15
